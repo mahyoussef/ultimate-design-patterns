3da39463daaff41680f208c5b4b9e37f5354f15d
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-03-02 18:39:11 +0200

    edited oder class, price is mandatory now

--- dart_lang/lib/a_solid/helper_classes/order.dart
+++ dart_lang/lib/a_solid/helper_classes/order.dart
@@ -1,7 +1,11 @@
 class Order {
   String? _name;
-  double? _price;
+  double _price;
+
+  Order(String? name, double price)
+      : _name = name,
+        _price = price;
 
   String? get name => _name;
-  double? get price => _price;
+  double get price => _price;
 }

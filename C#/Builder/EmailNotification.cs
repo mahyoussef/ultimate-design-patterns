c7297f4e16442ff62da5bea7551db4ddeaa47c94
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-03-06 18:56:16 +0200

    Add Builder Desgin Pattern

---
+++ C#/Builder/EmailNotification.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Builder
+{
+    public class EmailNotification
+    {
+        private readonly string content;
+        private readonly string timestamp;
+        private readonly string sender;
+        private readonly string recipient;
+
+        private EmailNotification(EmailNotificationBuilder emailNotificationBuilder)
+        {
+            this.content = emailNotificationBuilder.Content;
+            this.sender = emailNotificationBuilder.Sender;
+            this.timestamp = emailNotificationBuilder.Timestamp;
+            this.recipient = emailNotificationBuilder.Recipient;
+        }
+
+        public string Content => content;
+        public string Timestamp => timestamp;
+        public string Sender => sender;
+        public string Recipient => recipient;
+
+        public interface NotificationBuilder
+        {
+            EmailNotificationBuilder SetContent(string content);
+            EmailNotificationBuilder SetSender(string sender);
+            EmailNotificationBuilder SetRecipient(string recipient);
+            EmailNotificationBuilder SetTimestamp(string timestamp);
+        }
+
+        public class EmailNotificationBuilder : NotificationBuilder
+        {
+            public string Content { get; private set; }
+            public string Timestamp { get; private set; }
+            public string Sender { get; private set; }
+            public string Recipient { get; private set; }
+
+            public EmailNotificationBuilder SetContent(string content)
+            {
+                this.Content = content;
+                return this;
+            }
+
+            public EmailNotificationBuilder SetSender(string sender)
+            {
+                this.Sender = sender;
+                return this;
+            }
+
+            public EmailNotificationBuilder SetRecipient(string recipient)
+            {
+                this.Recipient = recipient;
+                return this;
+            }
+
+            public EmailNotificationBuilder SetTimestamp(string timestamp)
+            {
+                this.Timestamp = timestamp;
+                return this;
+            }
+
+            public EmailNotification Build()
+            {
+                return new EmailNotification(this);
+            }
+        }
+    }
+}
---
+++ C#/Builder/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class ProductService
+    {
+        public async Task<HttpResponseMessage> GetProducts()
+        {
+            var uri = new Uri("https://dummyjson.com/products/1");
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            request.Headers.Add("key1", "value1");
+            request.Headers.Add("key2", "value2");
+            request.Timeout = TimeSpan.FromSeconds(10);
+
+            using (var httpClient = new HttpClient())
+            {
+                return await httpClient.SendAsync(request);
+            }
+        }
+    }
+}

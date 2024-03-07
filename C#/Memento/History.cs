00c993fa91f148ad1bcb7151d605587e2ca668a0
mazidan77 <moga4arab@gmail.com>
2024-03-07 20:16:51 +0200

    Add Memento Design Pattern

---
+++ C#/Memento/History.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet.Memento
+{
+    public class History
+    {
+        private Stack<TextEditorState> prevStates;
+
+        public History()
+        {
+            prevStates = new Stack<TextEditorState>();
+        }
+
+        public void SaveHistoryState(TextEditorState textEditorState)
+        {
+            prevStates.Push(textEditorState);
+        }
+
+        public TextEditorState Undo()
+        {
+            if (prevStates.Count > 0)
+            {
+                return prevStates.Pop();
+            }
+            return null;
+        }
+    }
+}

cce2fc21c416c4268b414c2d0329772226bcce5f
Yousef Wael <youssef20171702080@cis.asu.edu.eg>
2024-03-09 23:10:57 +0200

    Added exercise for Proxy Pattern

---
+++ C#/Proxy/Exercise/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Proxy.Exercise;
+
+public sealed class User
+{
+    public string Name { get; }
+
+    public bool HasPermission { get => false; }
+
+    public User(string username)
+        => Name = username;
+}
---
+++ C#/Proxy/Exercise/VideoServiceProxy.cs
@@ -0,0 +1,26 @@
+﻿namespace Proxy.Exercise;
+
+public sealed class VideoServiceProxy : IMediaStreaming
+{
+    private readonly VideoService _service;
+    private readonly User _user;
+
+    public VideoServiceProxy(User user)
+    {
+        _service = new VideoService();
+        _user = user;
+    }
+
+    public void PlayVideo(string videoId)
+    {
+        if (_user.HasPermission)
+        {
+            _service.PlayVideo(videoId);
+            Console.WriteLine($"Logged user interaction: {_user.Name} watched video {videoId}");
+        }
+        else
+        {
+            Console.WriteLine($"Access denied for video: {videoId}");
+        }
+    }
+}

7e09b9302f02384f0428aedc1d54bbe49cf92cf8
Yousef Wael <youssef20171702080@cis.asu.edu.eg>
2024-03-20 23:08:58 +0200

    Add Flyweight Design Pattern

---
+++ C#/Flyweight/Enemy.cs
@@ -0,0 +1,23 @@
+﻿namespace Flyweight;
+
+public sealed class Enemy : IPlayer
+{
+    public string DisplayName { get; } = "Enemy Bot";
+    public int AttackPower { get; }
+    public int HealthBar { get; }
+    public Weapon Weapon { get; set; } = new("Rifle", 0);
+
+    public Enemy(int attackPower, int healthBar)
+    {
+        Console.WriteLine("Creating enemy...");
+        AttackPower = attackPower;
+        HealthBar = healthBar;
+    }
+
+
+    public void AssignWeapon(Weapon weapon)
+        => Weapon = weapon;
+
+    public void Attack()
+        => Console.WriteLine($"Player: {DisplayName} is attacking with weapon: {Weapon.Name} with base attack damage: {AttackPower}");
+}
---
+++ C#/Flyweight/Exercise/Character.cs
@@ -0,0 +1,20 @@
+﻿namespace Flyweight.Exercise;
+
+public sealed class Character : ITextElement
+{
+    private char _character;
+    private Font _font;
+    private Color _color;
+
+    public Character(char character, Font font, Color color)
+    {
+        _character = character;
+        _font = font;
+        _color = color;
+    }
+
+    public void Render(int xPosition, int yPosition)
+    {
+        // Render the character at position (xPosition, yPosition) with font and color
+    }
+}

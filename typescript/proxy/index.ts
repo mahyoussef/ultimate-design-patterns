31aed00a7b29cfc55fddfcb32c9336ff061f13f1
Mohamed <msaad@goatar.com>
2024-04-06 16:55:53 +0200

    [TS - Proxy pattern]: Add native JS implementation

--- typescript/proxy/index.ts
+++ typescript/proxy/index.ts
@@ -1,9 +1,13 @@
 import { DummyJsonCachingProxy } from "./dummy-json-caching-proxy";
+import { nativeProxy } from "./native-js-proxy";
 
 const apiProxy = new DummyJsonCachingProxy();
 
 (async function main() {
-  console.log("Calling for the first time.................................................................................................\n");
+  console.log('With api proxy:\n');
+  console.log(
+    "Calling for the first time.................................................................................................\n"
+  );
   let startTime = new Date().getTime();
 
   let products = await apiProxy.getAllProducts();
@@ -12,14 +16,42 @@ const apiProxy = new DummyJsonCachingProxy();
 
   console.log("completion Time: ", getCompletionTime(startTime) + "ms\n\n");
 
-  console.log("Calling for the second time.................................................................................................\n");
+  console.log(
+    "Calling for the second time.................................................................................................\n"
+  );
   startTime = new Date().getTime();
 
   products = await apiProxy.getAllProducts();
   recipes = await apiProxy.getAllRecipes();
   console.log(products, recipes);
 
-  console.log("completion Time: ", getCompletionTime(startTime) + "ms");
+  console.log("completion Time: ", getCompletionTime(startTime) + "ms\n\n");
+
+
+
+  console.log("With native proxy:\n");
+  console.log(
+    "[NATIVE] Calling for the first time.................................................................................................\n"
+  );
+  startTime = new Date().getTime();
+  products = await nativeProxy.getAllProducts();
+  recipes = await nativeProxy.getAllRecipes();
+  console.log(products, recipes);
+  
+  console.log("completion Time: ", getCompletionTime(startTime) + "ms\n\n");
+  
+  console.log(
+    "[NATIVE] Calling for the second time.................................................................................................\n"
+  );
+  startTime = new Date().getTime();
+  products = await nativeProxy.getAllProducts();
+  recipes = await nativeProxy.getAllRecipes();
+  console.log(products, recipes);
+  
+  console.log("[NATIVE] completion Time: ", getCompletionTime(startTime) + "ms");
+})();
+
+(async function mainWithNativeProxy() {
 })();
 
 function getCompletionTime(startTime: number) {

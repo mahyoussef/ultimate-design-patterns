9283336d6efef0e965156ab298d64ac00266dcee
Mohamed Ali <mohamed.ali.21@outlook.com>
2024-05-10 20:29:09 +0300

    Template Method Coding Exercise

---
+++ C#/Template/Exercise/FHDVideoEditingPreset.cs
@@ -0,0 +1,10 @@
+﻿using Template.Exercise.Abstruction;
+
+namespace Template.Exercise;
+internal class FHDVideoEditingPreset : VideoEditingPreset
+{
+    protected override void RenderVideo()
+    {
+        Console.WriteLine("Rendering FHD video...");
+    }
+}
